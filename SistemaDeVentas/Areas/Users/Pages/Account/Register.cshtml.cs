816fc46429cabb1e1d55cbeb1a5197c362e70cc2
quiqueac <82471440+quiqueac@users.noreply.github.com>
2022-01-27 12:09:33 -0600

    Propiedades que contribuirán en el desarrollo de la interfaz

--- SistemaDeVentas/Areas/Users/Pages/Account/Register.cshtml.cs
+++ SistemaDeVentas/Areas/Users/Pages/Account/Register.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SistemaDeVentas.Areas.Users.Models;
 
 namespace SistemaDeVentas.Areas.Users.Pages.Account
 {
@@ -11,6 +12,13 @@ namespace SistemaDeVentas.Areas.Users.Pages.Account
     {
         public void OnGet()
         {
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+        public class InputModel : InputModelRegister
+        {
+
         }
     }
 }
